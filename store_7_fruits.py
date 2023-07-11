511049ef10718e08155ee1517975821579fce3ab
Dhwaj <dhwajdhruvkar@gmail.com>
2023-07-11 20:12:20 +0530

    DSA

---
+++ store_7_fruits.py
@@ -0,0 +1,9 @@
+f1 = input("Enter Fruit 1 ")
+f2 = input("Enter Fruit 2 ")
+f3 = input("Enter Fruit 3 ")
+f4 = input("Enter Fruit 4 ")
+f5 = input("Enter Fruit 5 ")
+f6 = input("Enter Fruit 6 ")
+f7 = input("Enter Fruit 7 ")
+myfruitlist = [f1, f2, f3 ,f4 ,f5 ,f6 ,f7]
+print(myfruitlist)
