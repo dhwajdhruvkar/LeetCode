2242e975c0cdb74beb21fd50faa5a4c38291bd90
Dhwaj <dhwajdhruvkar@gmail.com>
2023-07-13 20:12:20 +0530

    DSA

---
+++ sumoflist.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter the number 1: "))
+n2 = int(input("Enter the number 2: "))
+n3 = int(input("Enter the number 3: "))
+n4 = int(input("Enter the number 4: "))
+myList = [n1, n2, n3, n4]
+print(myList)
+print(myList[0] + myList[1] + myList[2] + myList[3]) #it sums the input list
+print(sum(myList)) #it also sums the input list but it the sum function in the above line we just added it manually
