83994804fd844dbf50bb2f46468859c9078c1bb9
Dhwaj <dhwajdhruvkar@gmail.com>
2023-07-16 20:12:20 +0530

    DSA

---
+++ bitwise.py
@@ -0,0 +1,16 @@
+a = int(input("enter number 1 "))
+b = int(input("enter number 2 "))
+ 
+print("all the bitwise operators") 
+
+print("a & b =", a & b)
+ 
+print("a | b =", a | b)
+
+print("a >> b", a >> b)
+
+print("a << b", a << b)
+
+print("~a =", ~a)
+
+print("a ^ b =", a ^ b)
