4c465f4a307c6ca5fbdff100e40f9a3073e0bfdb
Dhwaj <dhwajdhruvkar@gmail.com>
2023-07-21 20:12:20 +0530

    DSA

---
+++ findprimeornot_usingloops.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the value of number \n"))
+prime = True
+
+for i in range(2, num):
+    if(num%i == 0):
+        print = False
+        break
+if (prime):
+    print("The given number is prime")
+else:
+    print("The given number is not prime")
