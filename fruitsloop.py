c5a6dfe6c2206c11dffb444b5de17cecae76118d
Dhwaj <dhwajdhruvkar@gmail.com>
2023-07-22 20:12:20 +0530

    DSA

---
+++ fruitsloop.py
@@ -0,0 +1,11 @@
+#fruits = ['Banana', 'Apple', 'Mango']
+#i = 0
+#while(i<len(fruits)):
+#    print(fruits[i])
+#    i = i + 1
+
+#This is using for loop and above code is written in while loop
+fruits = ['Banana', 'Apple', 'Mango']
+
+for item in fruits:
+    print(item)
