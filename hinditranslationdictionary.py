b0f1cf2ea6ebb87dc970c64752059446bd7fdc0d
Dhwaj <dhwajdhruvkar@gmail.com>
2023-07-30 20:12:20 +0530

    DSA

---
+++ hinditranslationdictionary.py
@@ -0,0 +1,9 @@
+myDict = {
+    "Pankha": "fan",
+    "Kamra" : "Room",
+    "Kursi" : "Chair",
+}
+
+print("Options are ", myDict.keys())
+a = input("Enter the Hindi word\n")
+print("The meaning of your word is: \n", myDict[a])
