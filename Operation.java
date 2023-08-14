cefd634a4f5d709520cf9624cb1b8dfa57d86c43
Dhwaj <dhwajdhruvkar@gmail.com>
2023-08-14 20:12:20 +0530

    DSA

---
+++ Operation.java
@@ -0,0 +1,10 @@
+
+public class Operation {
+    int type;
+    Object argument;
+
+    Operation(int type, Object argument) {
+        this.type = type;
+        this.argument = argument;
+    }
+}
