9ea969e6b258617a4aaea83faa5f38b876985fe7
Dhwaj <dhwajdhruvkar@gmail.com>
2023-08-16 20:12:20 +0530

    DSA

---
+++ romanToInt.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int romanToInt(String s){
+        int sum = 0;
+        int prev = 0;
+        for(int i = s.length()-1; i >= 0; i--){
+            int curr =
+        }
+    }
+}
