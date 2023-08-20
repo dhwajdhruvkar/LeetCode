632b4125d196e35ebe6b0ac4e1999a9c751626ac
Dhwaj <dhwajdhruvkar@gmail.com>
2023-08-20 20:12:20 +0530

    DSA

---
+++ 6students.py
@@ -0,0 +1,10 @@
+Student1 = int(input("Enter the Marks of Student 1: "))
+Student2 = int(input("Enter the Marks of Student 2: "))
+Student3 = int(input("Enter the Marks of Student 3: "))
+Student4 = int(input("Enter the Marks of Student 4: "))
+Student5 = int(input("Enter the Marks of Student 5: "))
+Student6 = int(input("Enter the Marks of Student 6: "))
+
+marks = [Student1, Student2, Student3, Student4, Student5, Student6]
+marks.sort()
+print(marks)
