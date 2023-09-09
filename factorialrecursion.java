f02c440b621e4409e7915f0572987be34c50f46c
Dhwaj <dhwajdhruvkar@gmail.com>
2023-09-09 20:12:20 +0530

    DSA

---
+++ factorialrecursion.java
@@ -0,0 +1,16 @@
+public class factorialrecursion {
+    public static int factorial(int n){
+        if(n == 1 || n == 0){
+            return 1;
+        }
+
+        int fact_nm1 = factorial(n-1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = factorial(n);
+        System.out.println(ans);
+    }
+}
